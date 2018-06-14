d06a3a08f750e13622279ce18e3d07d06b54c835
Karthik Nandam <nandam@promolta.com>
2018-06-14 16:11:05 +0530

    changes

--- setup_env.sh
+++ setup_env.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-mkdir dependencies/ext_script
-mv dependencies/make_cli_script_file.py dependencies/ext_script/.
-touch dependencies/ext_script/__init__.py
-git clone git@github.com:knan02/scripts.git
-#touch __init__.py
+touch __init__.py
+touch code/__init__.py
 
